0f92e107edf5951fcda0387a1bc067d95c6d83dd
student <student@I-208-PC08>
2025-02-27 18:00:33 +0300

    filter

---
+++ Filters/ScheduleFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Filters
+{
+    public class ScheduleFilter
+    {
+        public int PrepodId { get; set; }
+
+        public int CafedraId { get; set; }
+
+        public int SubjectId { get; set; }
+    }
+}
