b77e320ad86cc336c2a8a11f42b6a9357e85834d
fivt kt <kti207208fivt@gmail.com>
2025-02-27 19:27:14 +0300

    4lab

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Filters;
 using WebApplication1.Interfaces;
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ScheduleController : ControllerBase
     {
@@ -15,6 +16,22 @@ namespace WebApplication1.Controllers
         {
             _logger = logger;
             _scheduleService = scheduleService;
+        }
+
+        [HttpPost(Name = "GetSchedule")]
+        public async Task<IActionResult> GetSchedule(int scheduleId, CancellationToken cancellationToken)
+        {
+            var schedule = await _scheduleService.GetScheduleAsync(scheduleId, cancellationToken);
+
+            return Ok(schedule);
+        }
+
+        [HttpPost(Name = "GetFilteredSchedules")]
+        public async Task<IActionResult> GetFilteredSchedules(ScheduleFilter scheduleFilter, CancellationToken cancellationToken)
+        {
+            var schedule = await _scheduleService.GetFilteredSchedules(scheduleFilter, cancellationToken);
+
+            return Ok(schedule);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Databases;
+using WebApplication1.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PrepodDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddServices();
 
 var app = builder.Build();
 
