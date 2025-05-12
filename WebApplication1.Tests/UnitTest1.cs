030875d045d2adf38275938888e97d96b2c9bfe3
Leonid <leonid-zotikov@mail.ru>
2025-05-12 09:27:30 +0300

    hgfhdfghdf

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var schd = new Schedule() { };
+
+            Assert.True(true);
+        }
+    }
+}
